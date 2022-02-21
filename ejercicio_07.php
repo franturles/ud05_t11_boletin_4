215ff74f8946f9cf594719701e92101214cc124f
franturles <kikofran35@hotmail.com>
2022-02-21 09:12:35 +0000

    No terminado

--- ejercicio_07.php
+++ ejercicio_07.php
@@ -7,5 +7,17 @@
         <h3>Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle do-while</h3>
     </head>
     <body>
+    <form action="ejercicio_07.php" enctype='multipart/form-data' method='post'>
+        <p>Numero: <input type='number' name='numero' min=0/></p>
+        <p><input type='submit' value='Submit'/></p>
+    </from>
+    <?php
+        $Numero=$_REQUEST["numero"];
+if(strlen($Numero) < 5){
+
+    }else{
+    echo "Tiene que ser de 5 cifras";
+}
+?>
     </body>
 </html>
