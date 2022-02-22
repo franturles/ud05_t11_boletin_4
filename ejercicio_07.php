6d4cc6a7e22822927fe44a4cf951e1f1fc12e2af
franturles <kikofran35@hotmail.com>
2022-02-22 08:25:15 +0000

    Corregir

--- ejercicio_07.php
+++ ejercicio_07.php
@@ -3,8 +3,11 @@
     <title>UD05_T11_boletin_4</title>
         <meta charset='UTF-8'>
         <h1>UD05_T11</h1>
-        <h2>Ejercicio 6</h2>
-        <h3>Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle do-while</h3>
+        <h2>Ejercicio 7</h2>
+        <h3>Realiza el control de acceso a una caja fuerte. La combinación será un número de 4 cifras. El
+programa nos pedirá la combinación para abrirla. Si no acertamos, se nos mostrará el mensaje
+“Lo siento, esa no es la combinación” y si acertamos se nos dirá “La caja fuerte se ha abierto
+satisfactoriamente”. Tendremos cuatro oportunidades para abrir la caja fuerte.</h3>
     </head>
     <body>
     <form action="ejercicio_07.php" enctype='multipart/form-data' method='post'>
@@ -12,11 +15,24 @@
         <p><input type='submit' value='Submit'/></p>
     </from>
     <?php
-        $Numero=$_REQUEST["numero"];
-if(strlen($Numero) < 5){
-
+    $_SESSION["intentos"]= 0;
+    session_start();
+if($_SESSION["intentos"] <= 4){        
+        $Numero=isset($_REQUEST["numero"])?$_REQUEST["numero"]:"";
+        $Clave=2222;
+    if(strlen($Numero) <= 4){
+        if($Numero == $Clave){
+            echo "Contraseña Correcta";
+            $_SESSION["intentos"]= 0;
+        }else{
+            echo "Contraseña es incorrecta";
+            $_SESSION["intentos"]= $_SESSION["intentos"] + 1;
+        }
     }else{
-    echo "Tiene que ser de 5 cifras";
+        echo "Tiene que ser de 5 cifras";
+    }
+}else{
+    echo "Numero de intentos terminado";
 }
 ?>
     </body>
