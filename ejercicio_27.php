64287f9432d40c8f420d89ffba615b525b4d1d0a
franturles <kikofran35@hotmail.com>
2022-03-09 21:08:51 +0000

    Corregir

---
+++ ejercicio_27.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+    <title>UD05_T11_boletin_4</title>
+        <meta charset='UTF-8'>
+        <h1>UD05_T11</h1>
+        <h2>Ejercicio 27</h2>
+        <h3>Escribe un programa que muestre, cuente y sume los múltiplos de 3 que hay entre 1 y un número
+leído por teclado.</h3>
+    </head>
+    <body>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method='post'>
+        <p>Numero: <input type='number' name='numero' min=0/></p>
+        <p><input type='submit' value='Submit'/></p>
+    </from>
+    <?php
+    $Numero = $_REQUEST["numero"];
+    for ($i=1; $i <= $Numero; $i++) { 
+        $Multiplos = $i*3;
+        echo "</br>$Multiplos";
+    }
+    
+    
+    
+    ?>
